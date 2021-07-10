a2b2335b4faae5a53a07130d8766b9f907c6350c
“yangkang” <913967742@qq.com>
2021-07-10 14:43:18 +0800

    fix:目录

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React By Github
+          Learn React By Github!Github Pages!
         </a>
       </header>
     </div>
